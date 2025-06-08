12417f8cecac6e4a7b79990565c250f0946bbc45
Giuliano Varriale <giuliano.oca@gmail.com>
2025-06-08 17:21:47 +0200

    Fix eslint errors

--- app/actions/update-form.ts
+++ app/actions/update-form.ts
@@ -3,6 +3,7 @@
 import prisma from "@/lib/prisma";
 import { redirect } from "next/navigation";
 import { getCurrentUser } from "../repositories/current-user-repository";
+import { FormStructure } from "@/types";
 
 export async function updateForm(form: FormStructure) {
   const currentUser = await getCurrentUser();
--- lib/session-utils.ts
+++ lib/session-utils.ts
@@ -1,33 +1,36 @@
 import { getCurrentUser } from "@/app/repositories/current-user-repository";
-import { createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({
+  NextResponse.next({
     request,
   });
-  const supabase = createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value),
-          );
-          supabaseResponse = NextResponse.next({
-            request,
-          });
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options),
-          );
-        },
-      },
-    },
-  );
+
+  // CODE NOT BEING USED. SHOULD BE REVISITED
+  // CHECK https://supabase.com/docs/guides/auth/server-side/nextjs
+  //
+  // const supabase = createServerClient(
+  //   process.env.SUPABASE_URL!,
+  //   process.env.SUPABASE_ANON_KEY!,
+  //   {
+  //     cookies: {
+  //       getAll() {
+  //         return request.cookies.getAll();
+  //       },
+  //       setAll(cookiesToSet) {
+  //         cookiesToSet.forEach(({ name, value }) =>
+  //           request.cookies.set(name, value),
+  //         );
+  //         supabaseResponse = NextResponse.next({
+  //           request,
+  //         });
+  //         cookiesToSet.forEach(({ name, value, options }) =>
+  //           supabaseResponse.cookies.set(name, value, options),
+  //         );
+  //       },
+  //     },
+  //   },
+  // );
 
   // Do not run code between createServerClient and
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
