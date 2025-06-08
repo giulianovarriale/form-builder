b8b067b40de874598041e49d37dd9b3844b5eb7e
Giuliano Varriale <giuliano.oca@gmail.com>
2025-06-08 18:23:49 +0200

    Fix session middleware

--- app/repositories/current-user-repository.ts
+++ app/repositories/current-user-repository.ts
@@ -1,8 +1,11 @@
 import { createClient } from "@/lib/supabase";
+import { createServerClient } from "@supabase/ssr";
 
-export async function getCurrentUser() {
-  const supabase = await createClient();
-  const { data } = await supabase.auth.getUser();
+export async function getCurrentUser(
+  supabase?: ReturnType<typeof createServerClient>,
+) {
+  const supabaseClient = supabase ?? (await createClient());
+  const { data } = await supabaseClient.auth.getUser();
 
   return data.user;
 }
--- lib/session-utils.ts
+++ lib/session-utils.ts
@@ -1,42 +1,40 @@
 import { getCurrentUser } from "@/app/repositories/current-user-repository";
+import { createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-  const supabaseResponse = NextResponse.next({
+  let supabaseResponse = NextResponse.next({
     request,
   });
 
-  // CODE NOT BEING USED. SHOULD BE REVISITED
-  // CHECK https://supabase.com/docs/guides/auth/server-side/nextjs
-  //
-  // const supabase = createServerClient(
-  //   process.env.SUPABASE_URL!,
-  //   process.env.SUPABASE_ANON_KEY!,
-  //   {
-  //     cookies: {
-  //       getAll() {
-  //         return request.cookies.getAll();
-  //       },
-  //       setAll(cookiesToSet) {
-  //         cookiesToSet.forEach(({ name, value }) =>
-  //           request.cookies.set(name, value),
-  //         );
-  //         supabaseResponse = NextResponse.next({
-  //           request,
-  //         });
-  //         cookiesToSet.forEach(({ name, value, options }) =>
-  //           supabaseResponse.cookies.set(name, value, options),
-  //         );
-  //       },
-  //     },
-  //   },
-  // );
+  const supabase = createServerClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value),
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options),
+          );
+        },
+      },
+    },
+  );
 
   // Do not run code between createServerClient and
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
   // IMPORTANT: DO NOT REMOVE auth.getUser()
-  const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser(supabase);
 
   if (
     !currentUser &&
